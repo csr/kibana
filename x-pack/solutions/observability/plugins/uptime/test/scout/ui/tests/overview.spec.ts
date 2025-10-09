d04117e8e6047f1f0d477e27d4e410df3f84cc8e
Cesare de Cal <cesare.decal@elastic.co>
2025-10-09 15:06:59 +0200

    reset overview.spec.ts

--- x-pack/solutions/observability/plugins/uptime/test/scout/ui/tests/overview.spec.ts
+++ x-pack/solutions/observability/plugins/uptime/test/scout/ui/tests/overview.spec.ts
@@ -12,7 +12,7 @@ const UPTIME_HEARTBEAT_DATA =
 const DEFAULT_NAVIGATION_SEARCH =
   'dateRangeEnd=2019-09-11T19:40:08.078Z&dateRangeStart=2019-09-10T12:40:08.078Z';
 
-test.describe('overview page', () => {
+test.describe('overview page', { tag: ['@ess', '@svlOblt'] }, () => {
   test.beforeAll(async ({ esArchiver }) => {
     await esArchiver.loadIfNeeded(UPTIME_HEARTBEAT_DATA);
   });
